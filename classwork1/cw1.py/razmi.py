a775fdd315a044302a14a82fb90e850fb0753201
nata-goa <bajelidzenata0@gmail.com>
2026-04-24 19:24:38 +0400

    day 82

---
+++ classwork1/cw1.py/razmi.py
@@ -0,0 +1,16 @@
+#integer 
+#float 
+#string
+#boolean
+
+#მომხმარებლისგან მიიღებ რიცხვს და უნდა გაიგო ლუწია თუ კენტი, თუ ლუჭია დავჭრთ even თუ კენტია odd
+
+num = int(input("enter a number : "))
+
+if num % 2 == 0:
+    print("even")
+
+else:
+    print("odd") 
+    
+    
